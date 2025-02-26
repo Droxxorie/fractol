1c1926143d17130dfaa009b088645f43179ed2b5
root <root@Eloi.>
2025-02-26 21:39:15 +0100

    rm; removed forbiden function memset

--- include/fractol.h
+++ include/fractol.h
@@ -23,7 +23,6 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <string.h>
 //* ------------------------------------------------------------------------*//
 
 //* Defines
